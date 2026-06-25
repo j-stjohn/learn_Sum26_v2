1729dfb5980dca5d7cd5776e70a602e31c5a86cb
Jacob <jacob.st.john.work@gmail.com>
2026-06-25 16:59:15 +0000

    modified:   C Code/week3/scrabble2.c "Added in random letter generation for ./scrabble2 r"

--- C Code/week3/scrabble2.c
+++ C Code/week3/scrabble2.c
@@ -1,5 +1,6 @@
 // Making a scrabble game 
-
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 char word[30];
@@ -10,39 +11,74 @@ char fours[] = {'f','F','h','H','v','V','w','W','y','Y'};
 char fives[] = {'k','K'};
 char eight[] = {'j','J','x','X'};
 char tens[] = {'q','Q','z','Z'};
+
+#define ONES_SIZE (sizeof(ones) / sizeof(ones[0]))
+#define TWOS_SIZE (sizeof(twos) / sizeof(twos[0]))
+#define THREES_SIZE (sizeof(threes) / sizeof(threes[0]))
+#define FOURS_SIZE (sizeof(fours) / sizeof(fours[0]))
+#define FIVES_SIZE (sizeof(fives) / sizeof(fives[0]))
+#define EIGHT_SIZE (sizeof(eight) / sizeof(eight[0]))
+#define TENS_SIZE (sizeof(tens) / sizeof(tens[0]))
+
 int sum = 0;
-int scrabble(word);
-int main(int argc, char argv[]){
-	int n = 0;
-	if (argc == 2) {
-	
-	}
-	else {
-	printf("Enter your word: ");
-	scanf("%s", word);
-	scrabble(word);
-	}
-}
-int scrabble(word) {
-	while (word[n] != '\0') {
-		for (int i = 0; i < sizeof(ones); i++) {
-			if (word[n] == ones[i]) { 
-				word[n] = 1; }
-			if (word[n] == twos[i]) {
-				word[n] = 2; }
-			if (word[n] == threes[i]) {
-				word[n] = 3; }
-			if (word[n] == fours[i]) {
-				word[n] = 4; }
-			if (word[n] == fives[i]) {
-				word[n] = 5; }
-			if (word[n] == eight[i]) {
-				word[n] = 8; }
-			if (word[n] == tens[i]) {
-				word[n] = 10; } 
+char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+int scrabble(char word[]);
+
+int main(int argc, char *argv[]) {
+	if (argc == 2 && argv[1][0] == 'r') {
+		// Generate random 15-character string
+		char random_chars[16];
+		srand(time(NULL));
+		
+		for (int i = 0; i < 15; i++) {
+			int idx = rand() % 52;  // 26 lowercase + 26 uppercase
+			random_chars[i] = letters[idx];
 		}
-		sum = sum + word[n];
-       		n++; }
-	printf("Total Score: %i\n",sum);
+		random_chars[15] = '\0';
+		
+		printf("Your letters: %s\n", random_chars);
+		printf("Make a word from these letters: ");
+		scanf("%s", word);
+		scrabble(word);
+	} else {
+		printf("Enter your word: ");
+		scanf("%s", word);
+		scrabble(word);
+	}
+	return 0;
+}
+int scrabble(char word[]) {
+	for (int i = 0; word[i] != '\0'; i++) {
+		char c = word[i];
+		int points = 0;
+		
+		// Check which point value this character belongs to
+		for (int j = 0; j < ONES_SIZE; j++) {
+			if (c == ones[j]) { points = 1; break; }
+		}
+		for (int j = 0; j < TWOS_SIZE; j++) {
+			if (c == twos[j]) { points = 2; break; }
+		}
+		for (int j = 0; j < THREES_SIZE; j++) {
+			if (c == threes[j]) { points = 3; break; }
+		}
+		for (int j = 0; j < FOURS_SIZE; j++) {
+			if (c == fours[j]) { points = 4; break; }
+		}
+		for (int j = 0; j < FIVES_SIZE; j++) {
+			if (c == fives[j]) { points = 5; break; }
+		}
+		for (int j = 0; j < EIGHT_SIZE; j++) {
+			if (c == eight[j]) { points = 8; break; }
+		}
+		for (int j = 0; j < TENS_SIZE; j++) {
+			if (c == tens[j]) { points = 10; break; }
+		}
+		
+		sum += points;
+	}
+	printf("Total Score: %i\n", sum);
+	return sum;
 }
 
