8905cacd2072c550aff5789c35a30a08dd9beae6
j-stjohn <jacob.st.john.work@gmail.com>
2026-07-01 23:47:32 -0400

    modified:   C Code/week2/class/array.c

--- C Code/week2/class/array.c
+++ C Code/week2/class/array.c
@@ -5,7 +5,7 @@ int main(void)
 	int scores[3];
 	scores[0] = 72;
 	scores[1] = 73;
-	scores[2] = 33;
+	scores[2] = 43;
 
 	// Print Average
 	printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
